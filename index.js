bed18fbde06193f957ba3c7e6329d6a72f55ade4
reshma768 <144494726+reshma768@users.noreply.github.com>
2023-12-06 10:01:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
-const http = require('http');
+const http = require('http'); 
 const fs = require('fs');
 const path = require('path');
-const { MongoClient } = require('mongodb');
-
-const PORT = 5959;
-const MONGODB_URI = "mongodb+srv://rshai10:R16661666@cluster0.yxifel9.mongodb.net/?retryWrites=true&w=majority";
-
-http.createServer(async (req, res) => {
+http.createServer((req, res)=>{
     console.log(req.url);
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    } else if (req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    } else if (req.url === '/api') {
-        try {
-            const data = await retrieveDataFromMongoDB();
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(data));
-        } catch (error) {
-            console.error(error);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        }
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end("<h1> 404 Nothing is here </h1>");
+    if(req.url ==='/'){ 
+
+       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(content)
+       })
+
+    }else if(req.url ==='/styles.css'){ 
+
+        fs.readFile(path.join(__dirname,'public','styles.css'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'text/css'})
+            res.end(content)
+        })
+
+    }else if(req.url ==='/api'){
+
+        fs.readFile(path.join(__dirname,'public','db.json'),(err,content)=>{
+            if (err) throw err ;
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(content)
+        })
+
+    }else{
+
+        res.end("<h1>404 Nothing is here</h1>")
+
     }
-}).listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-async function retrieveDataFromMongoDB() {
-    const client = new MongoClient(MONGODB_URI);
-
-    try {
-        await client.connect();
-        const databaseName = "food";
-        const collectionName = "nutrition";
-        const cursor = client.db(databaseName).collection(collectionName).find({});
-        const results = await cursor.toArray();
-        const formattedResults = [];
-
-        results.forEach(result => {
-            result.Food.forEach(foodItem => {
-                formattedResults.push({ _id: result._id, ...foodItem });
-            });
-        });
-
-        return formattedResults;
-    } catch (error) {
-        console.error("Error during MongoDB operation:", error);
-        throw error; // Rethrow the error to propagate it to the calling function
-    } finally {
-        await client.close();
-    }
-}
+}).listen(7818,()=>console.log("Server is running"));
